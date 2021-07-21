5b45bd59b890ab2b0762b915d6888e79f8094193
Mélina <purple.melinaa@gmail.com>
2021-07-21 23:13:59 +0200

    Ajout du header et footer + charte graphique améliorée

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import "./App.css";
 import { useState } from "react";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
 
 function App() {
   const [price1, setPrice1] = useState(null);
@@ -7,11 +9,11 @@ function App() {
 
   return (
     <div>
+      <Header />
       <div className="container">
-
     
       <div class="container-vertical">
-        <h1>Tesla config</h1>
+
         <div class="vehicle">
           <h2>Sélectionnez votre véhicule</h2>
           <div class="price1" onClick={() => setPrice1(90700)} style={{ backgroundColor: price1 === 90700 ? "rgb(186, 149, 221)" : ""}}>Grande autonomie - 90700 €</div>
@@ -22,10 +24,14 @@ function App() {
           <div class="price3" onClick={() => setPrice2(0)} style={{ backgroundColor: price2 === 0 ? "rgb(186, 149, 221)" : ""}}>Blanc nacré multicouche - 0 € </div>
           <div class="price4" onClick={() => setPrice2(1000)} style={{ backgroundColor: price2 === 1000 ? "rgb(186, 149, 221)" : ""}}>Noir uni - 1000 €</div>
         </div>
-        <div className="total" >{price1 + price2} € <span><button onClick={() => alert('Yeah, je roule en Tesla :)')}>Buy !</button></span></div>
+        <div className="total" >{price1 + price2} € 
+         {price1 + price2 > 0 ?   <span><button onClick={() => alert('Yeah, je roule en Tesla :)')}>Buy !</button></span> : ''}
+      
+        </div>
         
       </div>
     </div>
+    <Footer />
     </div>
   );
 }
---
+++ src/components/Header.js
@@ -0,0 +1,13 @@
+const Header = () => {
+    return (
+      <div className="header">
+        <div className="logo">
+          <i class="fas fa-car fa-3x"></i>
+        </div>
+        <div className="title">Tesla config</div>
+      </div>
+    );
+  };
+  
+  export default Header;
+  
